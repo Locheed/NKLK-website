b8987d1242d7a16c54faebfad1460386cd4ee44d
Mika Vesasto <locheed@gmail.com>
2017-04-15 02:42:28 +0300

    Page almost finished. Responsive done

--- src/app/stats-soldiername/stats-soldiername.component.ts
+++ src/app/stats-soldiername/stats-soldiername.component.ts
@@ -12,15 +12,18 @@ export class StatsSoldiernameComponent implements OnInit {
   private pageTitle: string = "Stats by Soldier Name";
   private errorMessage: string;
   stats: IStats[];
+  private isLoading = false;
 
   constructor(private _StatsService: StatsService) { }
 
   
   getByName(soldierName: string) {
+    this.isLoading = true;
     this._StatsService.getBySoldierName(soldierName)
-        .subscribe((...stats) => this.stats = stats,
-              error => this.errorMessage = <any>error);
-            
+        .subscribe((...stats) => {
+                      this.stats = stats;
+                      this.isLoading = false;
+        }, error => this.errorMessage = <any>error);
   }
 
   ngOnInit() {
